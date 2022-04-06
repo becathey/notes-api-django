a1e28ac63b156bddf76f87969b8d16b75fc03999
Bill Cathey <billcathey@hotmail.com>
2022-04-05 23:35:56 -0400

    Create Note model

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Note
 
-# Register your models here.
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("title", "text")
+
+admin.site.register(Note, NoteAdmin)
--- note/models.py
+++ note/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=120)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
