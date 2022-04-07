dbf128ccb2a1c7cfee03dae1c98794e68143556f
Bill Cathey <billcathey@hotmail.com>
2022-04-06 23:25:48 -0400

    Create view

--- note/views.py
+++ note/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import NoteSerializer
+from .models import Note
 
-# Create your views here.
+class NoteView(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.all()
