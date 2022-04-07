3a1fdb493a76d3e1907c093575d7e97ee677f601
Bill Cathey <billcathey@hotmail.com>
2022-04-06 23:22:55 -0400

    Create NoteSerializer

---
+++ note/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Note
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ('id', 'title', 'text')
